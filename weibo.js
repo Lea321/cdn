4c01be8c7ee55f307220fd0f08f5234de9b7f4e9
Leonus <75648501+Lea321@users.noreply.github.com>
2022-07-24 18:08:36 +0800

    Add files via upload

--- weibo.js
+++ weibo.js
@@ -1,5 +1,11 @@
-fetch('https://weibo-top-api.vercel.app/api').then(data => data.json()).then(data => {
-    let html = '<div id="weibo-container">'
+try {
+    let w = document.getElementById('weibo').clientWidth
+    if (w) {
+        weibo();
+    }
+} catch (error) {}
+
+function weibo() {
     let hotness = {
         '爆': 'weibo-boom',
         '热': 'weibo-hot',
@@ -10,15 +16,32 @@ fetch('https://weibo-top-api.vercel.app/api').then(data => data.json()).then(dat
         '剧': 'weibo-jyzy',
         '综': 'weibo-jyzy'
     }
-    for (let item of data) {
+    let html = '<div id="weibo-container">'
+    let data = JSON.parse(localStorage.getItem('weibo'));
+    let nowTime = Date.now();
+    let ls;
+    if (data == null || nowTime - data.time > 600000) {
+        getData();
+        return
+    } else {
+        ls = JSON.parse(data.ls)
+    };
+    for (let item of ls) {
         html += '<div class="weibo-list-item"><div class="weibo-hotness ' + hotness[(item.hot || '荐')] + '">' + (item.hot || '荐') + '</div>' +
             '<span class="weibo-title"><a title="' + item.title + '"href="' + item.url + '" target="_blank" rel="external nofollow noreferrer">' + item.title + '</a></span>' +
             '<div class="weibo-num"><span>' + item.num + '</span></div></div>'
     }
     html += '</div>'
-    try {
-        document.getElementById('weibo').getElementsByClassName('item-content')[0].innerHTML = html
-    } catch (error) {}
-}).catch(function(error) {
-    console.log(error);
-});
+    document.getElementById('weibo').getElementsByClassName('item-content')[0].innerHTML = html
+}
+
+function getData() {
+    fetch('https://weibo-top-api.vercel.app/api').then(data => data.json()).then(data => {
+        data = { time: Date.now(), ls: JSON.stringify(data) }
+        localStorage.setItem('weibo', JSON.stringify(data))
+        console.log('微博热搜获取完成！');
+    }).then(() => {
+        weibo();
+    });
+
+}
